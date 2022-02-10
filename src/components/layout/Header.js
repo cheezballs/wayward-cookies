cd8aa57e987ae2c2df65fcafa8b27dfe14b6b57d
Matt Roberts <sweetcrapmatt@gmail.com>
2022-02-10 16:20:12 -0600

    More updates.

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -5,12 +5,12 @@ import { faBars } from '@fortawesome/free-solid-svg-icons';
 
 export default function Header(props) {
 	return (
-		<nav className="navbar navbar-expand-lg justify-content-between" id="mainNav">
-			<div className="container">
+		<nav className="navbar navbar-expand-md justify-content-between" id="mainNav">
+			<div className="container-fluid">
 				<button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target=".dual-nav">
 					<FontAwesomeIcon className="ms-1" icon={faBars} />
 				</button>
-				<div className="navbar-collapse collapse dual-nav w-50 order-1 order-md-0">
+				<div className="navbar-collapse collapse dual-nav w-50 order-1 order-md-0 left">
 					<ul className="navbar-nav ms-auto">
 						<li className="nav-item active">
 							<Link to="/home" className="nav-link">Home</Link>
@@ -23,13 +23,16 @@ export default function Header(props) {
 						</li>
 					</ul>
 				</div>
-				<div className="navbar-brand mx-auto order-0 order-md-1">
+				<div className="navbar-brand mx-auto d-none d-md-block order-md-1">
 					<img src="/images/logo_text.gif" alt="Wayward Cookies logo" id="logo" />
 				</div>
-				<div className="navbar-collapse collapse dual-nav w-50 order-2">
+				<div className="navbar-brand mx-auto d-md-none order-md-1 w-75">
+					<img src="/images/text-logo.png" alt="Wayward Cookies logo" width="100%" id="text-logo" />
+				</div>
+				<div className="navbar-collapse collapse dual-nav w-50 order-2 right">
 					<ul className="nav navbar-nav me-auto">
 						<li className="nav-item">
-							<Link to="/testimonials" className="nav-link">Testimonials</Link>
+							<Link to="/testimonials" className="nav-link">Clients</Link>
 						</li>
 						<li className="nav-item">
 							<Link to="/contact" className="nav-link">Contact</Link>
